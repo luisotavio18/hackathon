e9f71044258df891d62419e993e61c4261b59568
Rodrigo <wtfrodriguu@gmail.com>
2024-11-24 14:36:32 -0300

    hackathon pronto

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,31 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const cors = require('cors');
-const postFuncionarios = require('./routes/funcionariosRouter');
-const postCategoriakits = require('./routes/categoriakitsRouter');
-const postKits = require('./routes/kitsRouter');
-const postInstrumentos = require('./routes/instrumentosRouter');
+
+// Importação dos roteadores
+const funcionariosRouter = require('./routes/funcionariosRouter');
+const categoriakitsRouter = require('./routes/categoriakitsRouter');
+const kitsRouter = require('./routes/kitsRouter');
+const router = require('./routes/cadastroRouter');
+const instrumentosRouter = require('./routes/instrumentosRouter');
+const videoRouter = require('./routes/videoRouter'); // Corrigido para o roteador de vídeos
+
 const app = express();
 
+// Configurações do servidor
 app.set('port', process.env.PORT || 3005);
+
+// Middlewares globais
 app.use(cors());
 app.use(express.json());
-app.use('/api', postFuncionarios);
-app.use('/api', postCategoriakits);
-app.use('/api', postInstrumentos);
-app.use('/api', postKits);
 
+// Rotas
+app.use('/api/funcionarios', funcionariosRouter);
+app.use('/api/categoriakits', categoriakitsRouter);
+app.use('/api/kits', kitsRouter);
+app.use('/api/instrumentos', instrumentosRouter);
+app.use('/api', router);
+app.use('/api/videos', videoRouter); // Adicionando a rota para vídeos
+
+// Exportação do app
 module.exports = app;
