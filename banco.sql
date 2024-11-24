cb10e6f0b65c627223ab6fd1f37fe64907158752
Luis <luisotavio0553@gmail.com>
2024-11-23 21:26:38 -0300

    projeto inicial

---
+++ banco.sql
@@ -0,0 +1,21 @@
+create database bancodedados;
+use bancodedados;
+
+create table funcionarios(
+	id int primary key auto_increment,
+	nome varchar(255),
+    cpf varchar(255),
+    endereco varchar(255),
+    credencial varchar(255),
+    cargo varchar(255)
+);
+
+create table instrumentos(
+	id int primary key auto_increment,
+	nome varchar(255)
+);
+
+drop table instrumentos, funcionarios;
+
+select * from funcionarios;
+select * from instrumentos;
